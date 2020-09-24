4c08c3249faff949d75a23a10640ffe43bf87c90
Sai Pavan Kumar <pavannpt@outlook.com>
2020-09-24 12:54:49 -0400

    corrected syntax

--- ModernWeb.Database/PRIME/prime_workflows.js
+++ ModernWeb.Database/PRIME/prime_workflows.js
@@ -1,60 +1,67 @@
 db.createCollection("PRIME_WORKFLOWS", {
     validator: {
-        $jsonScema: {
-            bsonType: Object,
+        $jsonSchema: {
+            bsonType: "object",
             required: ["WorkflowId", "SourceSystemId", "WorkflowDescription", "InputFilePath", "DocumentType", "IsActive"],
             properties: {
                 WorkflowId: {
                     bsonType: "int",
-                    descrption: "Must be an integer and cannot be null"
+                    description: "Must be an integer and cannot be null"
                 },
                 SourceSystemId: {
                     bsonType: "int",
-                    descrption: "Must be an integer and cannot be null"
+                    description: "Must be an integer and cannot be null"
                 },
                 WorkflowDescription: {
                     bsonType: "string",
-                    descrption: "Must be a string and cannot be empty"
+                    description: "Must be a string and cannot be empty",
+                    maxLength: 250
                 },
                 InputFilePath: {
                     bsonType: "string",
-                    descrption: "Must be a string and cannot be empty"
+                    description: "Must be a string and cannot be empty",
+                    maxLength: 250
                 },
                 DocumentType: {
                     bsonType: "string",
-                    descrption: "Must be a string and cannot be empty"
+                    description: "Must be a string and cannot be empty",
+                    maxLength: 10
                 },
                 StateCode: {
                     bsonType: "string",
-                    descrption: "Must be a string and can contain valid 2 digit state code"
+                    description: "Must be a string and can contain valid 2 digit state code",
+                    maxLength: 3
                 },
                 IsSingleRun: {
                     bsonType: "bool",
-                    descrption: "Must be a boolean"
+                    description: "Must be a boolean"
                 },
                 TriggerBatch: {
                     bsonType: "string",
-                    descrption: "Must be a string"
+                    description: "Must be a string",
+                    maxLength: 70
                 },
                 IsActive: {
                     bsonType: "bool",
-                    descrption: "Must be a boolean and cannot be null"
+                    description: "Must be a boolean and cannot be null"
                 },
                 CreatedBy: {
                     bsonType: "string",
-                    descrption: "Must be a string"
+                    description: "Must be a string",
+                    maxLength: 100
                 },
                 CreatedDate: {
                     bsonType: "date",
-                    descrption: "Must be an ISODATE"
+                    description: "Must be an ISODATE"
                 },
                 UpdatedBy: {
                     bsonType: "string",
-                    descrption: "Must be a string"
+                    description: "Must be a string",
+                    maxLength: 100
                 },
                 UpdatedDate: {
                     bsonType: "date",
-                    descrption: "Must be an ISODATE"
+                    description: "Must be an ISODATE"
                 },
             }
         }
